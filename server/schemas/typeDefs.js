e3abab969b58a6a07e96736128f50c743fa7b08e
Heidi Mailahn <92544035+hmailahn@users.noreply.github.com>
2022-04-22 17:08:08 -0500

    added auth, book, and mutation types

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -14,6 +14,29 @@ const typeDefs = gql`
       bookCount: Int
       savedBooks: [Book]
   }
+
+  type Book {
+      bookId: String
+      authors: [String]
+      description: String
+      title: String
+      image: String
+      link: String
+  }
+
+  type Auth {
+    token: ID!
+    user: User
+  }
+
+  type Mutation {
+      login(email: String!, password: String!): Auth
+      addUser(username: String!, email: String! password: String!) Auth
+    #   (Look into creating what's known as an input type to handle all of these parameters!)
+      saveBook(authors: [String!], description: String!, title: String!, bookId: String!, image: String!, link: String!) User
+      removeBook(bookId: String!) User
+  }
+
   `;
 
 // export the typeDefs
@@ -21,41 +44,3 @@ module.exports = typeDefs;
 
 
 
-// typeDefs.js: Define the necessary Query and Mutation types:
-
-// Query type:
-//       me: Which returns a User type.
-
-
-
-// Mutation type:
-    // login: Accepts an email and password as parameters; returns an Auth type.
-
-    // addUser: Accepts a username, email, and password as parameters; returns an Auth type.
-
-    // saveBook: Accepts a book author's array, description, title, bookId, image, and link as parameters; returns a User type. (Look into creating what's known as an input type to handle all of these parameters!)
-
-    // removeBook: Accepts a book's bookId as a parameter; returns a User type.
-
-
-
-
-// Book type:
-    // bookId (Not the _id, but the book's id value returned from Google's Book API.)
-
-    // authors (An array of strings, as there may be more than one author.)
-
-    // description
-
-    // title
-
-    // image
-
-    // link
-
-
-// Auth type:
-
-    // token
-    
-    // user (References the User type.)   
